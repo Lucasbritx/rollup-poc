4e311c21d511dc3bbc7ce6df2a4daa030897ed5f
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-07-15 22:34:14 -0300

    fix: scss tests config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 /** @type {import("jest").Config} **/
-module.exports = {
+export default {
   testEnvironment: "jsdom",
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  preset: 'ts-jest/presets/default-esm',
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": ["ts-jest", { useESM: true }],
     "^.+\\.(js|jsx)$": "babel-jest",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
   testRegex: "/__tests__/.*\\.(test|spec)\\.[jt]sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
-    "\\.(css)$": "identity-obj-proxy"
+    "\\.(css|scss)$": "identity-obj-proxy"
   },
   collectCoverage: true,
   coverageDirectory: "coverage",
